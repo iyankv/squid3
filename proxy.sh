ab60463bd9e3f28738a3f5403464bb16dd3f1902
iyan.kv <spencer.saril.7@gmail.com>
2016-07-16 17:23:18 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -8,6 +8,6 @@ fi
 aptitude -y install squid3
 rm -f /etc/squid3/squid.conf
 
-wget -P /etc/squid3/ "asdsadsadasdsad"
+wget -P /etc/squid3/ "https://raw.githubusercontent.com/iyankv/squid3/master/conf/squid.conf"
 sed -i "s/ipserver/$IP/g" /etc/squid3/squid.conf
 service squid3 restart
